01e213e0fa2f7ddf1b5d90567a77ed186e6991d9
fsabu <fsabu@DESKTOP-T4EQUBV>
2024-06-05 20:39:03 +0200

    exanem

---
+++ src/porfavor/porfavor.java
@@ -0,0 +1,18 @@
+package porfavor;
+
+public class porfavor {
+
+	public static void main(String[] args) {
+		System.out.println("porfavor te lo pido");
+		System.out.println("porfavor te lo pido");
+		System.out.println("porfavor te lo pido");
+		System.out.println("porfavor te lo pido");
+		System.out.println("porfavor te lo pido");
+		System.out.println("porfavor te lo pido");
+		System.out.println("porfavor te lo pido");
+		System.out.println("porfavor te lo pido");
+		System.out.println("porfavor te lo pido");
+		System.out.println("porfavor te lo pido");
+	}
+
+}
